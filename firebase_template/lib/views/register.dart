b095b440e5045c8d25279bff461b97170c342ad8
Jay <jvanord@indasysllc.com>
2020-01-25 17:42:03 -0500

    added registration and signin pages

---
+++ firebase_template/lib/views/register.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/authentication/bloc.dart';
+import '../blocs/registration/bloc.dart';
+import '../services/authentication_service.dart';
+
+class RegistrationPage extends StatelessWidget {
+  final AuthService _authService;
+
+  RegistrationPage({Key key, @required AuthService authService})
+      : assert(authService != null),
+        _authService = authService,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Center(
+        child: BlocProvider<RegistrationBloc>(
+          create: (context) => RegistrationBloc(authService: _authService),
+          child: RegistrationForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationForm extends StatefulWidget {
+  State<RegistrationForm> createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  RegistrationBloc _registerBloc;
+
+  bool get isPopulated =>
+      _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isRegisterButtonEnabled(RegistrationState state) {
+    return state.isFormValid && isPopulated && !state.isSubmitting;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _registerBloc = BlocProvider.of<RegistrationBloc>(context);
+    _emailController.addListener(_onEmailChanged);
+    _passwordController.addListener(_onPasswordChanged);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<RegistrationBloc, RegistrationState>(
+      listener: (context, state) {
+        if (state.isSubmitting) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Registering...'),
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              ),
+            );
+        }
+        if (state.isSuccess) {
+          BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
+          Navigator.of(context).pop();
+        }
+        if (state.isFailure) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Registration Failure'),
+                    Icon(Icons.error),
+                  ],
+                ),
+                backgroundColor: Colors.red,
+              ),
+            );
+        }
+      },
+      child: BlocBuilder<RegistrationBloc, RegistrationState>(
+        builder: (context, state) {
+          return Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              child: ListView(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.email),
+                      labelText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    autovalidate: true,
+                    validator: (_) {
+                      return !state.isEmailValid ? 'Invalid Email' : null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    autocorrect: false,
+                    autovalidate: true,
+                    validator: (_) {
+                      return !state.isPasswordValid ? 'Invalid Password' : null;
+                    },
+                  ),
+                  RegistrationButton(
+                    onPressed: isRegisterButtonEnabled(state)
+                        ? _onFormSubmitted
+                        : null,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onEmailChanged() {
+    _registerBloc.add(
+      EmailChanged(email: _emailController.text),
+    );
+  }
+
+  void _onPasswordChanged() {
+    _registerBloc.add(
+      PasswordChanged(password: _passwordController.text),
+    );
+  }
+
+  void _onFormSubmitted() {
+    _registerBloc.add(
+      Submitted(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ),
+    );
+  }
+}
+
+class RegistrationButton extends StatelessWidget {
+  final VoidCallback _onPressed;
+
+  RegistrationButton({Key key, VoidCallback onPressed})
+      : _onPressed = onPressed,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      onPressed: _onPressed,
+      child: Text('Register'),
+    );
+  }
+}
---
+++ firebase_template/lib/views/signin.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../blocs/authentication/bloc.dart';
+import '../blocs/signin/bloc.dart';
+import '../services/authentication_service.dart';
+import 'register.dart';
+
+class LoginScreen extends StatelessWidget {
+  final AuthService _authService;
+
+  LoginScreen({Key key, @required AuthService authService})
+      : assert(authService != null),
+        _authService = authService,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: BlocProvider<LoginBloc>(
+        create: (context) => LoginBloc(injectedAuthService: _authService),
+        child: LoginForm(authService: _authService),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  final AuthService _authService;
+
+  LoginForm({Key key, @required AuthService authService})
+      : assert(authService != null),
+        _authService = authService,
+        super(key: key);
+
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginBloc _loginBloc;
+
+  AuthService get _authService => widget._authService;
+
+  bool get isPopulated =>
+      _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isLoginButtonEnabled(SigninState state) {
+    return state.isFormValid && isPopulated && !state.isSubmitting;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc = BlocProvider.of<LoginBloc>(context);
+    _emailController.addListener(_onEmailChanged);
+    _passwordController.addListener(_onPasswordChanged);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginBloc, SigninState>(
+      listener: (context, state) {
+        if (state.isFailure) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Login Failure'), Icon(Icons.error)],
+                ),
+                backgroundColor: Colors.red,
+              ),
+            );
+        }
+        if (state.isSubmitting) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Logging In...'),
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              ),
+            );
+        }
+        if (state.isSuccess) {
+          BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
+        }
+      },
+      child: BlocBuilder<LoginBloc, SigninState>(
+        builder: (context, state) {
+          return Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Image.asset('assets/flutter_logo.png', height: 200),
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.email),
+                      labelText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autovalidate: true,
+                    autocorrect: false,
+                    validator: (_) {
+                      return !state.isEmailValid ? 'Invalid Email' : null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    autovalidate: true,
+                    autocorrect: false,
+                    validator: (_) {
+                      return !state.isPasswordValid ? 'Invalid Password' : null;
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        LoginButton(
+                          onPressed: isLoginButtonEnabled(state)
+                              ? _onFormSubmitted
+                              : null,
+                        ),
+                        GoogleLoginButton(),
+                        CreateAccountButton(authService: _authService),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onEmailChanged() {
+    _loginBloc.add(
+      EmailChanged(email: _emailController.text),
+    );
+  }
+
+  void _onPasswordChanged() {
+    _loginBloc.add(
+      PasswordChanged(password: _passwordController.text),
+    );
+  }
+
+  void _onFormSubmitted() {
+    _loginBloc.add(
+      LoginWithCredentialsPressed(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final VoidCallback _onPressed;
+
+  LoginButton({Key key, VoidCallback onPressed})
+      : _onPressed = onPressed,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      onPressed: _onPressed,
+      child: Text('Login'),
+    );
+  }
+}
+
+class GoogleLoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      icon: Icon(FontAwesomeIcons.google, color: Colors.white),
+      onPressed: () {
+        BlocProvider.of<LoginBloc>(context).add(
+          LoginWithGooglePressed(),
+        );
+      },
+      label: Text('Sign in with Google', style: TextStyle(color: Colors.white)),
+      color: Colors.redAccent,
+    );
+  }
+}
+
+class CreateAccountButton extends StatelessWidget {
+  final AuthService _authService;
+
+  CreateAccountButton({Key key, @required AuthService authService})
+      : assert(authService != null),
+        _authService = authService,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text(
+        'Create an Account',
+      ),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) {
+            return RegistrationPage(authService: _authService);
+          }),
+        );
+      },
+    );
+  }
+}

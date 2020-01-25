e1b193b3116918342d8605e16ae0b8fe98696017
Jay <jvanord@indasysllc.com>
2020-01-25 16:34:23 -0500

    wired in splash and home screens w/o login flow

--- firebase_template/lib/main.dart
+++ firebase_template/lib/main.dart
@@ -1,90 +1,49 @@
+import 'package:firebase_template/views/home.dart';
+import 'package:firebase_template/views/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_template/blocs/authentication/bloc.dart';
+import 'package:firebase_template/blocs/dev_bloc_delegate.dart';
+import 'package:firebase_template/services/authentication_service.dart';
 
-void main() => runApp(TemplateApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // init before run
+  BlocSupervisor.delegate = DevBlocDelegate(); // use in-dev delegate to observe
+  final AuthService authService = AuthService();
+  runApp(BlocProvider<AuthenticationBloc>(
+    create: (context) => AuthenticationBloc(authService)..add(AppStarted()),
+    child: TemplateApp(injectedAuthService: authService),
+  ));
+}
 
 class TemplateApp extends StatelessWidget {
+  final AuthService _authService;
+  TemplateApp({Key key, @required AuthService injectedAuthService})
+      : assert(injectedAuthService != null),
+        _authService = injectedAuthService,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Template',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: 'Firebase Template',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+            if (state is Uninitialized) {
+              return SplashScreen();
+            }
+            if (state is Authenticated) {
+              return HomePage(user: state.user);
+            }
+            return Container(
+              child: Center(
+                child: Text('State failure on load.'),
+              ),
+            );
+          },
+        ));
   }
 }
